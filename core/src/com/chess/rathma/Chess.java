9957f252a8284be6c2173ad21d2a3136e04ef162
wrathofrathma <max21509@archlinux.us>
2016-05-20 08:17:08 -0400

    Playing around with fonts, apparently the gdx-freetype extension lets me generate fonts on the fly from .tff rather than use bitmap images. This is much more condusive to later variation

--- core/src/com/chess/rathma/Chess.java
+++ core/src/com/chess/rathma/Chess.java
@@ -2,26 +2,41 @@ package com.chess.rathma;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
 public class Chess extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	FreeTypeFontGenerator generator;
+    FreeTypeFontGenerator.FreeTypeFontParameter param;
+    String text;
+    BitmapFont font12;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture(Gdx.files.internal("chessboard544.png"));
+        generator = new FreeTypeFontGenerator(Gdx.files.internal("TTF/SourceCodePro-Regular.ttf"));
+        param = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        param.size=12;
+        param.color = Color.WHITE;
+        font12 = generator.generateFont(param);
+        text = "Hallo world";
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
+        font12.draw(batch, text, 20,20);
 		batch.end();
 	}
 }
