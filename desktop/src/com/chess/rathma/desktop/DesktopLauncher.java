5a65e420e98b5c7881a61c5765c76d0d496b550b
wrathofrathma <max21509@archlinux.us>
2016-05-20 06:15:14 -0400

    Initial commit & most of what I think is necessary to the project. New to gradle so I'm not sure what I should add

---
+++ desktop/src/com/chess/rathma/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.chess.rathma.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.chess.rathma.Chess;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Chess(), config);
+	}
+}
